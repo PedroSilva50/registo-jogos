d6b3a705c7212109ed95baeeea5eca8fbfa70211
PedroSilva50 <silva_50@live.com.pt>
2026-08-31 17:42:16 +0100

    Update CACHE_NAME to version 2.8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'coachfolio-v2.7.9';
+const CACHE_NAME = 'coachfolio-v2.8';
 
 self.addEventListener('install', (e) => {
   // Retiramos o skipWaiting automático para a app não reiniciar a meio de um jogo.
